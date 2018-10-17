57ecfab16d0c2450ac10bb0aa6b5be6909fd4a1b
Danielle Dookie <danielledookie90@gmail.com>
2018-10-17 00:46:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 let currentUser = 'danielle'
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() +'!'
+let shortGreeting = "Welcome, "
